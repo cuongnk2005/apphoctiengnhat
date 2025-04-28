ca5aa9dbf52046062dbb5815e4b4163ee28c17d0
cuongnk2005 <cuongnk.23it@vku.udn.vn>
2025-04-28 08:09:09 +0700

    save local changes

--- app/src/main/java/com/example/myproject/Repository/TopicRepository.kt
+++ app/src/main/java/com/example/myproject/Repository/TopicRepository.kt
@@ -1,9 +1,14 @@
 package com.example.myproject.Repository
 
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.myproject.Model.Topic
+import kotlinx.coroutines.tasks.await
 
 class TopicRepository {
-    private val db = FirebaseFirestore.getInstance()
-    private var topicCollection = db.collection("Topic")
-
+//    private val db = FirebaseFirestore.getInstance()
+//    private var topicCollection = db.collection("Topic")
+//suspend fun getAllTopics(): List<Topic>{
+//    // lay ve document
+//    val snapshot = topicCollection.get().await()
+//
+//}
 }
