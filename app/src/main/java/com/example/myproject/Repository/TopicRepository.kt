d7557e92545552288cdc0af4db10f17f6e8bceb7
cuongnk2005 <cuongnk.23it@vku.udn.vn>
2025-04-28 15:38:55 +0700

    add lib

--- app/src/main/java/com/example/myproject/Repository/TopicRepository.kt
+++ app/src/main/java/com/example/myproject/Repository/TopicRepository.kt
@@ -1,14 +1,23 @@
 package com.example.myproject.Repository
 
+import android.util.Log
 import com.example.myproject.Model.Topic
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.tasks.await
 
 class TopicRepository {
-//    private val db = FirebaseFirestore.getInstance()
-//    private var topicCollection = db.collection("Topic")
-//suspend fun getAllTopics(): List<Topic>{
-//    // lay ve document
-//    val snapshot = topicCollection.get().await()
-//
-//}
+    private val db = FirebaseDatabase.getInstance()
+    private var topicsRef = db.getReference("topics")
+fun pushTopic(topic:Topic, onSuccess:()->Unit , onFailure:(Exception)->Unit ){
+ var newTopicRef = topicsRef.push()
+    newTopicRef.setValue(topic)
+        .addOnSuccessListener {
+            Log.d("TopicRepository", "Push Topic thành công")
+            onSuccess()
+        }
+        .addOnFailureListener{ expection ->
+            Log.e("TopicRepository", "Push Topic thất bại $expection ")
+            onFailure(expection)
+        }
+}
 }
